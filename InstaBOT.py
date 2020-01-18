1f1cc3e0f4f80b2f63564f8a32582e51a387834d
Mehmet D <55313337+Nerimb@users.noreply.github.com>
2020-01-18 23:49:41 +0200

    Add files via upload

---
+++ InstaBOT.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from time import sleep
+
+browser = webdriver.Chrome(executable_path=r"C:\webdrivers\chromedriver.exe")
+browser.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+class WPBot:
+    def__init__ = (self, username, pw):
+    sleep(2)
+webdriver.find_element_by_xpath(''//input[@name=\''username\'']'')\
+.send_keys(bucakprintshirt)
+webdriver.find_element_by_xpath(''//input[@name=\''password\'']'')\
+.send_keys(kardeslertisort15)
+webdriver.find_element_by_xpath(''//button[@type=\''submit\'']'')\
+.click()
+sleep(4)
+
+   WPBot('bucakprintshirt', 'kardeslertisort15')
+
