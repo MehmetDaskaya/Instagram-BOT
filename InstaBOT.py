ac2aa68c04d343a0692e83e2a88ada788d9f2115
Mehmet D <55313337+Nerimb@users.noreply.github.com>
2020-01-19 01:34:38 +0200

    Update InstaBOT.py

--- InstaBOT.py
+++ InstaBOT.py
@@ -7,12 +7,12 @@ class WPBot:
     def__init__ = (self, username, pw):
     sleep(2)
 webdriver.find_element_by_xpath(''//input[@name=\''username\'']'')\
-.send_keys(bucakprintshirt)
+.send_keys(name)
 webdriver.find_element_by_xpath(''//input[@name=\''password\'']'')\
-.send_keys(kardeslertisort15)
+.send_keys(password)
 webdriver.find_element_by_xpath(''//button[@type=\''submit\'']'')\
 .click()
 sleep(4)
 
-   WPBot('bucakprintshirt', 'kardeslertisort15')
+   WPBot('name', 'password')
 
