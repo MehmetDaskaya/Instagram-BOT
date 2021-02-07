e5f424f8e635519dd6ae6b735cde242c07c4c242
Mehmet D <55313337+Nerimb@users.noreply.github.com>
2021-02-07 23:41:55 +0300

    Selenium ile Instagram Takipçilerinizi Listelemek
    Bu instagram botu takipçilerinizi bir listeye almaya ve sıralamaya yarıyor.

--- InstaBOT.py
+++ InstaBOT.py
@@ -1,18 +1,37 @@
 from selenium import webdriver
-from time import sleep
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import pyautogui
+import time
 
-browser = webdriver.Chrome(executable_path=r"C:\webdrivers\chromedriver.exe")
-browser.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
-class WPBot:
-    def__init__ = (self, username, pw):
-    sleep(2)
-webdriver.find_element_by_xpath(''//input[@name=\''username\'']'')\
-.send_keys(name)
-webdriver.find_element_by_xpath(''//input[@name=\''password\'']'')\
-.send_keys(password)
-webdriver.find_element_by_xpath(''//button[@type=\''submit\'']'')\
-.click()
-sleep(4)
+username = input("hesabınızın ismi nedir?\n")                                                            #Giriş yapmak için hesap bilgilerini istiyor.
+pw = input("şifreniz nedir?\n")                                                                          #Giriş yapmak için hesap bilgilerini istiyor.
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-   WPBot('name', 'password')
+driver.get("http://instagram.com")
+time.sleep(5)
+driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
+time.sleep(1)
+driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(pw)
+time.sleep(1)
+driver.find_element_by_xpath('//button[@type="submit"]').click()
+time.sleep(5)
+driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[5]/span").click()    #xpath kodu ile tıklaması gereken yerleri buluyor
+time.sleep(3)
+driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[5]/div[2]/div[2]/div[2]/a[1]").click()
+time.sleep(10)
+driver.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/ul/li[2]/a").click()
+time.sleep(40)
 
+
+takipciler = driver.find_elements_by_css_selector(".FPmhX.notranslate._0imsa ")                          #Css kodundan username ile ilgili kod ile takipçileri buluyor.
+
+f= open("takipciler1.txt","w+")                                                                          #takipciler1.txt isimli bir not defteri klasörü oluşturuyor.
+
+for takipci in takipciler:
+    f.write(takipci.text + "\n")                                                                         #Sayfada yüklenen tüm takipçileri takipciler1.txt dosyasına listeliyor.
+
+f.close() 
+
+time.sleep(4)
